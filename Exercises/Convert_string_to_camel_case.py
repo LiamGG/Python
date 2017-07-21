bb20c8f098ffb3478ca827eff2a0f9510cbf9cb7
Liam <liamgalbraith@gmail.com>
2017-07-21 16:54:50 +0100

    Update Convert_string_to_camel_case.py
    Update comments and task

--- Exercises/Convert_string_to_camel_case.py
+++ Exercises/Convert_string_to_camel_case.py
@@ -1,6 +1,9 @@
 # Convert string to camel case
 # 5 kyu
 
+# Write a method/function so that converts dash/underscore delimited words into camel casing.
+# The first word within the output should be capitalized only if the original word was capitalized.
+
 def to_camel_case(text):
     # Replace all _ with -, split string into array at -
     words = text.replace('_','-').split('-')
