11f055de8e5e2364a808683e5794af61d74b8f95
Liam <liamgalbraith@gmail.com>
2017-07-21 16:49:55 +0100

    Update comments

--- Exercises/Are_they_the_same.py
+++ Exercises/Are_they_the_same.py
@@ -1,19 +1,20 @@
 # Are they the "same"?
 # 6 kyu
 
-# Given two arrays a and b write a function comp(array1, array2) that checks
+# Given two arrays a and b, write a function comp(array1, array2) that checks
 # whether the two arrays have the "same" elements, with the same multiplicities.
 # "Same" means, here, that the elements in b are the elements in a squared, 
 # regardless of the order.
 
 def comp(array1, array2):
     
-    # Check that both array1 and array2 are lists
+    # Check that both array1 and array2 are actually lists
     if isinstance(array1, list) and isinstance(array2, list):
     
-        # Sort the arrays into order
+        # Sort into order
         array1.sort()
         array2.sort()
+        
         # Loop over the values in array1, if the square of the value is not 
         # equal to the value in the same index in array 2, return False
         i = 0
