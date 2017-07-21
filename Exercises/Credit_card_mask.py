311fa381d3304fcf8faa6d08c7159cb6bf0809dd
Liam <liamgalbraith@gmail.com>
2017-07-21 16:56:12 +0100

    Update Credit_card_mask.py

--- Exercises/Credit_card_mask.py
+++ Exercises/Credit_card_mask.py
@@ -1,8 +1,9 @@
 # Credit card mask
 # 7 kyu
 
-def maskify(cc):
+# Your task is to write a function maskify, which changes all but the last four characters into '#'.
 
+def maskify(cc):
     if len(cc) <= 4:
         return cc
     else:
